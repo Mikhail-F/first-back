703e81c88d1399136268727ff0b166cb200b3dfd
Mikhail Tabunov <mtt@d-element.ru>
2023-05-03 12:14:36 +0500

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res: any) => {
+    let helloMessage = "Hello World!"
+  res.send(helloMessage)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
